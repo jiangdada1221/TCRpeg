94f3a9c2e7ba48cf631841150e744373f714b993
jiangdada1221 <jiangdada12344321@163.com>
2023-03-02 03:23:41 +0000

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 data_files_to_include = [('', ['README.md', 'LICENSE'])]
 setup(name='tcrpeg',
-      version='1.0.5',
+      version='1.0.6',
       description='Recovering the repertoire probability, generating and encoding CDR3 sequences',
       long_description='To be added',
       url='https://github.com/jiangdada1221/TCRpeg',
